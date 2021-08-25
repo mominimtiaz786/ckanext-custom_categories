f7bba4ba6aadc33ed1a5f66250c6d2ff6f1c3e2a
Momin Imtiaz <21100179@lums.edu.pk>
2021-08-25 22:55:41 +0500

    defined category validator

--- ckanext/custom_categories/plugin.py
+++ ckanext/custom_categories/plugin.py
@@ -1,25 +1,32 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
-categories_list = ["Science & Technology","Public Safety", "Manufactoring & Public Services","Agriculture, Food & Forests","Cities & Regions","Connectivity","Culture","Demography","Economy & Finance","Education","Environment & Energy","Government & Public Sector","Health","Housing & Public Services" ]
+CATEGORIES_LIST = ["Science & Technology","Public Safety", "Manufactoring & Public Services","Agriculture, Food & Forests","Cities & Regions","Connectivity","Culture","Demography","Economy & Finance","Education","Environment & Energy","Government & Public Sector","Health","Housing & Public Services" ]
 
 
 def all_categories():
     options_list = []
-    for opt in categories_list:
+    for opt in CATEGORIES_LIST:
         dict1 = {'text':opt,'value':opt}
         options_list.append(dict1)
     
     return options_list
 
+def valid_category(value):
+    if value in CATEGORIES_LIST:
+        return value
+    else:
+        raise tk.Invalid('Select a Valid Package Category')
+
 class Custom_CategoriesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
-    p.implements(p.IDatasetForm)
-    p.implements(p.IConfigurer)
-    # Declare that this plugin will implement ITemplateHelpers.
-    p.implements(p.ITemplateHelpers)
+    
+    p.implements(p.IValidators)
+    def get_validators(self):
+        return {
+            'custom_category_valid_category':valid_category
+        }    
     
     p.implements(p.IFacets)
-
     def dataset_facets(self, facets_dict, package_type):
         #facets_dict['groups'] = tk._('custom_text')
         #facets_dict['custom_text'] = facets_dict.pop('groups')
@@ -31,6 +38,8 @@ class Custom_CategoriesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
                 new_dict[key] = facets_dict[key]
         return new_dict
 
+
+    p.implements(p.IDatasetForm) 
     def is_fallback(self):
         # Return True to register this plugin as the default handler for
         # package types not handled by any other IDatasetForm plugin.
@@ -43,8 +52,10 @@ class Custom_CategoriesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
     def _modify_package_schema(self, schema):
         schema.update({
-            'category': [tk.get_validator('ignore_missing'),
-                            tk.get_converter('convert_to_extras')]
+            'category': [#tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras'),
+                            tk.get_validator('custom_category_valid_category')
+                            ]
         })
         return schema
 
@@ -67,16 +78,18 @@ class Custom_CategoriesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         return schema
     
     # IConfigurer
+    p.implements(p.IConfigurer)
     def update_config(self, config_):
         tk.add_template_directory(config_, 'templates')
         #toolkit.add_public_directory(config_, 'public')
         #toolkit.add_resource('fanstatic', 'custom_categories')
             
-
+    p.implements(p.ITemplateHelpers)
     def get_helpers(self):
         # Template helper function names should begin with the name of the
         # extension they belong to, to avoid clashing with functions from
         # other extensions.
         return {'custom_categories_list': all_categories                
                 }
+
 
